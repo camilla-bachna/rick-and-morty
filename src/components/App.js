9a3b7876e764faf84a1fb5006e831aa6d3abf67e
camilla-bachna <camilla.bachna@gmail.com>
2021-03-03 19:33:15 +0100

    add input and filter

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,29 @@ import React, { useEffect, useState } from 'react';
 import dataApi from '../services/dataApi';
 import CharactersList from './CharactersList';
 import logo from '../images/Rick_and_Morty_-_logo.png';
+import Filter from './Filter';
 
-console.log(dataApi());
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [name, setName] = useState('');
+
   useEffect(() => {
     dataApi().then((data) => setCharacters(data));
   }, []);
+
+  const handleFilter = (inputKey, inputValue) => {
+    if (inputKey === 'name') {
+      setName(inputValue);
+    }
+  };
+
   return (
     <>
       <header className="header">
         <img src={logo} alt="logo Rick and Morty" className="header__image" />
       </header>
       <main>
+        <Filter handleFilter={handleFilter} name={name} />
         <CharactersList characters={characters} />
       </main>
       <footer>
---
+++ src/components/Filter.js
@@ -0,0 +1,22 @@
+const Filter = (props) => {
+  const handleCharacterName = (event) => {
+    props.handleFilter(event.target.name, event.target.value);
+    console.log(event.target.name, event.target.value);
+  };
+
+  return (
+    <form className="form">
+      <label htmlFor="name">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="write a name here..."
+          onKeyUp={handleCharacterName}
+          className="form__input"
+        />
+      </label>
+    </form>
+  );
+};
+export default Filter;
